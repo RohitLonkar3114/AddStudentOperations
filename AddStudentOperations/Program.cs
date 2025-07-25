47f15b51cd3e16ba462a6dc8eb664476d50fcb45
RohitLonkar3114 <rohitlonkar3114@gmail.com>
2025-07-25 08:56:44 +0530

    Add project files.

---
+++ AddStudentOperations/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddStudentOperations
+{
+    public class Program
+    {
+        void AddStudent()
+        {
+            Console.WriteLine("Student Added Successfully");
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.AddStudent();
+            Console.ReadLine();
+        }
+    }
+}
